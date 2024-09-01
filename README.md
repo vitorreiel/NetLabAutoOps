c6fcaaff83270c5a4a64a6420de73a409aa6ab7c
vitorreiel <vitorreiel@hotmail.com>
2024-09-01 12:21:29 -0300

    feat: implementation of topology creation

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# automation-of-simulated-networks
-Automatização de redes simuladas na Nuvem
+# ⌨️ NetLabAutoOps 💻
+<br>
+
+O objetivo deste projeto é propor, de forma automatizada, a criação de topologias de rede SDN utilizando containers Docker e a ferramentar Containernet com implementação na nuvem AWS.
+
+<br>
+
+---
+<br>
+<br>
+
+<div align="center"><img width="150" height="150" src="https://cdn-icons-png.flaticon.com/512/564/564619.png"></img></div>
+
+<br>
+<br>
+
+> #### 🎯 Nota: É importante destacar que siga todas as instruções abaixo, para que não ocorra nenhum tipo de problema.
+
+<br>
+
+---
+
+<br>
+
+## 🔎 Pré-requisitos:
+<br>
+
+```sh
+✏️ 1° Requisito - Tenha acesso a uma conta AWS Academy;
+
+✏️ 2° Requisito - Tenha acesso a um terminal Linux com permissão de super usuário.
+```
+
+---
+
+<br>
+
+## 🔎 Execução:
+<br>
+
+**✏️ 1° Passo** - Clone este repositório em sua máquina local utilizando o comando abaixo no seu terminal:
+```sh
+git clone https://github.com/vitorreiel/NetLabAutoOps.git
+```
+<br>
+<br>
+
+**✏️ 2° Passo** - Acesse sua AWS Academy e inicie seu Lab. Para iniciar seu Lab, basta clicar na opção:
+```sh
+Start Lab
+```
+
+<br>
+<br>
+
+**✏️ 3° Passo** - Acesse sua AWS Academy e clique na opção "AWS Details". Feito isso, procure a informação "AWS CLI" e em seguida, clique na opção "Show". Com isso, copie toda sua AWS CLI, pois ela será usada posteriormente. Abaixo um exemplo de como seria uma AWS CLI:
+```sh
+[default] 
+aws_access_key_id=xxxxxxxxxxxxxxxxxx 
+aws_secret_access_key=xxxxxxxxxxxxxxxxxxxxxxxxx 
+aws_session_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+<br>
+<br>
+
+**✏️ 4° Passo** - Retorne para o terminal de sua máquina local e entre no repositório "NetLabAutoOps". Feito isso, utilize o editor de texto de sua preferência para editar o arquivo "aws_access". Com isso, cole sua AWS CLI copiada no passo anterior. Abaixo um exemplo de como entrar no repositório e editar o arquivo "aws_access":
+```sh
+cd NetLabAutoOps
+nano aws_access
+```
+
+>  🎯 **Nota:** Você pode apagar, se desejar, todo o conteúdo comentado no arquivo aws_access e depois colar sua AWS CLI. Como também, pode apenas colar na útlima linha do arquivo. Fica a sua escolha.
+<br>
+<br>
+
+**✏️ 5° Passo** - Por fim, execute o script. Abaixo o comando de execução do script:
+```sh
+./start.sh
+```
+
+<br>
+<br>
+
+---
+
+<br>
+
+#### 🎯 Nota: Os dados de capturados usado para análise comparativa das ferramentas de IaC, podem ser encontradas aqui:
+[🗂 NetLabAutoOps-Dataset 📈](https://github.com/vitorreiel/NetLabAutoOps-Dataset.git)
+
+<br>
+
+---
+
+<div style="display: inline_block;">
+
+   ![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
+
+</div>
+<div style="display: inline_block;">
+   <img height="34" width="34" hspace="7" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
+   <img height="34" width="30" hspace="7" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/terraform/terraform-original.svg" />
+   <img height="34" width="30" hspace="7" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" />
+</div>
--- start.sh
+++ start.sh
@@ -12,46 +12,43 @@ key_destino="utils/credentials/keypair.pem"
 echo -e "\n\033[1;33m- [ Atenção: É necessário que sua conta AWS Academy esteja iniciada! ] \033[0m"
 sleep 1
 echo -e '\n\033[1;33m- [ Atenção: É necessário que tenha colocado sua AWS CLI no arquivo "aws_access". ] \033[0m\n'
-sleep 1
+sleep 0.5
 echo -e "\n\033[1;36m- [ Digite: 1 - Para iniciar o processo criação automatizada de rede emulada. ] \033[0m"
-sleep 1
+sleep 0.5
 echo -e "\n\033[1;31m- [ Digite: 2 - Para deletar um cenário de rede emulada já existente. ] \033[0m\n"
+echo -e '\n\033[1;35m- [ Por favor, digite o valor correspondente: ] \033[0m'
 read confirmacao
 
 if [[ "$confirmacao" =~ ^(1|01)$ ]]; then
 
-  echo -e "\n\033[1;32m- [ Qual ferramenta deseja utilizar para criação do cenário? Digite apenas o número! ] \033[0m\n"
-  sleep 1
-  echo -e "\n\033[1;34m- [ 1 ] : Ansible \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 2 ] : Terraform \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 3 ] : Chef \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 4 ] : Puppet \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 5 ] : Pulumi \033[0m\n"
-  sleep 0.5
-  read iac
+    echo -e "\n\n\033[1;32m- [ Qual ferramenta deseja utilizar para criação do cenário? Digite apenas o número! ] \033[0m\n"
+    sleep 0.5
+    echo -e "\n\033[1;34m- [ 1 ] : Ansible \033[0m"
+    sleep 0.5
+    echo -e "\n\033[1;34m- [ 2 ] : Terraform \033[0m\n"
+    sleep 0.5
+    echo -e '\n\033[1;35m- [ Por favor, digite o valor correspondente: ] \033[0m'
+    read iac
 
-  echo -e "\n\033[1;32m- [ Digite apenas o número correspondente a topologia desejada! ] \033[0m\n"
-  sleep 1
-  echo -e "\n\033[1;34m- [ 1 ] : Topologia de Rede Linear \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 2 ] : Topologia de Rede Single \033[0m"
-  sleep 0.5
-  echo -e "\n\033[1;34m- [ 3 ] : Topologia de Rede Tree \033[0m"
-  sleep 0.5
-  read topologia
+    echo -e "\n\n\033[1;32m- [ Digite apenas o número correspondente a topologia desejada! ] \033[0m\n"
+    sleep 0.5
+    echo -e "\n\033[1;34m- [ 1 ] : Topologia de Rede Single \033[0m"
+    sleep 0.5
+    echo -e "\n\033[1;34m- [ 2 ] : Topologia de Rede Linear \033[0m"
+    sleep 0.5
+    echo -e "\n\033[1;34m- [ 3 ] : Topologia de Rede Tree \033[0m\n"
+    sleep 0.5
+    echo -e '\n\033[1;35m- [ Por favor, digite o valor correspondente: ] \033[0m'
+    read topologia
 
-  generate_python_code() {
-    local topology=$1
-    local num_switches_lvl1=$2
-    local num_switches_lvl2=$3
-    local num_hosts_per_switch_lvl2=$4
-    local num_switches=$2
-    local num_hosts=$3
-    local python_file="./utils/network-components/topology/topology.py"
+    generate_python_code() {
+        local topology=$1
+        local num_switches_lvl1=$2
+        local num_switches_lvl2=$3
+        local num_hosts_per_switch_lvl2=$4
+        local num_switches=$2
+        local num_hosts=$3
+        local python_file="./utils/network-components/topology/topology.py"
 
     # Cria um arquivo temporário para o código Python
     temp_file=$(mktemp)
@@ -74,6 +71,13 @@ def topology(args):
 EOF
 
     if [ "$topologia" == "1" ] || [ "$topologia" == "01" ]; then
+        echo "    S1 = net.addSwitch('S1')" >> $temp_file
+        for ((i=1; i<=$num_hosts; i++)); do
+            echo "    H$i = net.addDocker('H$i', mac='00:00:00:00:00:$(printf '%02x' $i)', ip='10.0.10.1$i/24', dimage=\"alpine-user:latest\")" >> $temp_file
+            echo "    net.addLink(S1, H$i)" >> $temp_file
+        done
+
+    elif [ "$topologia" == "2" ] || [ "$topologia" == "02" ]; then
         # Adicionando switches
         for ((i=1; i<=$num_switches; i++)); do
             echo "    S$i = net.addSwitch('S$i')" >> $temp_file
@@ -88,13 +92,6 @@ EOF
         # Conectando os switches em série
         for ((i=1; i<$num_switches; i++)); do
             echo "    net.addLink(S$i, S$((i + 1)))" >> $temp_file
-        done
-
-    elif [ "$topologia" == "2" ] || [ "$topologia" == "02" ]; then
-        echo "    S1 = net.addSwitch('S1')" >> $temp_file
-        for ((i=1; i<=$num_hosts; i++)); do
-            echo "    H$i = net.addDocker('H$i', mac='00:00:00:00:00:$(printf '%02x' $i)', ip='10.0.10.1$i/24', dimage=\"alpine-user:latest\")" >> $temp_file
-            echo "    net.addLink(S1, H$i)" >> $temp_file
         done
 
     elif [ "$topology" == "3" ] || [ "$topology" == "03" ]; then
@@ -139,72 +136,78 @@ EOF
     mv $temp_file $python_file
 }
 
-  case $topologia in
-  1|01)
-    while true; do
-      echo "Quantos switches você deseja criar?"
-      read num_switches
+    case $topologia in
+        1|01)
+            echo -e '\n\033[1;35m- [ Quantos hosts serão conectados ao switch central? ] \033[0m'
+            read num_hosts
+            num_switches=1
+    esac
 
-      echo "Quantos hosts serão conectados a esses switches?"
-      read num_hosts
-
-      # Verifica se a quantidade de switches e hosts é a mesma
-      if [ "$num_switches" -eq "$num_hosts" ]; then
-          break
-      else
-          echo "A quantidade de switches e hosts deve ser a mesma. Por favor, tente novamente."
-      fi
-    done
-  esac
-
-  case $topologia in
+    case $topologia in
     2|02)
-      echo "Quantos hosts serão conectados ao único switch?"
-      read num_hosts
-      num_switches=1
-  esac
+        while true; do
+            echo -e "\n\033[1;35m- [ Quantos switches devem ser criados? ] \033[0m"
+            read num_switches
+            echo -e "\n\033[1;35m- [ Quantos hosts devem ser criados? ] \033[0m"
+            read num_hosts
 
-  case $topologia in
-    3|03)
-    echo "Quantos switches você deseja no nível 1?"
-    read num_switches_lvl1
-    echo "Quantos switches você deseja no nível 2?"
-    read num_switches_lvl2
-    echo "Quantos hosts serão conectados a cada switch do nível 2?"
-    read num_hosts_per_switch_lvl2
-  esac
+            # Verifica se a quantidade de switches e hosts é a mesma
+            if [ "$num_switches" -eq "$num_hosts" ]; then
+                break
+            else
+                echo -e "\n\033[1;33m- [ A quantidade de switches e hosts devem ser a mesma na topologia Linear. Por favor, tente novamente. ] \033[0m"
+            fi
+        done
+    esac
 
-  if [ "$topologia" == "3" ] || [ "$topologia" == "03" ]; then
-      generate_python_code "$topologia" "$num_switches_lvl1" "$num_switches_lvl2" "$num_hosts_per_switch_lvl2"
-  else
-      generate_python_code "$topologia" "$num_switches" "$num_hosts"
-  fi
+    case $topologia in
+        3|03)
+        echo -e "\n\033[1;35m- [ Quantos switches você deseja no nível 1? ] \033[0m"
+        read num_switches_lvl1
+        echo -e "\n\033[1;35m- [ Quantos switches você deseja no nível 2? ] \033[0m"
+        read num_switches_lvl2
+        echo -e "\n\033[1;35m- [ Quantos hosts serão conectados a cada switch de nível 2? ] \033[0m"
+        read num_hosts_per_switch_lvl2
+    esac
 
-  case $iac in
-  1|01)
-    echo -e "\n\033[1;33m- [ Iniciando configurações da Infraestrutura. Aguarde! ] \033[0m\n"
-    sudo apt update -y > /dev/null 2>&1
-    sudo apt install git python3 python3-pip ansible -y > /dev/null 2>&1
-    pip install boto3 ansible-core==2.16.0 Jinja2==3.1.3 urllib3==1.26.5 > /dev/null 2>&1
-    ansible-galaxy collection install community.aws > /dev/null 2>&1
-    awk -v new_value_1="$aws_access_key" 'NR == 2 {print "aws_access_key: " new_value_1} NR != 2' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
-    awk -v new_value_2="$aws_secret_key" 'NR == 3 {print "aws_secret_key: " new_value_2} NR != 3' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
-    awk -v new_value_3="$aws_session_token" 'NR == 4 {print "aws_session_token: " new_value_3} NR != 4' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
-    echo -e "\033[1;32m- [ Dependências instaladas com Sucesso! ] \033[0m\n"
-    ansible-playbook -i automated-networks/ansible-playbook/hosts automated-networks/ansible-playbook/playbook.yaml
-    ip=$(awk '/ansible_host/ {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}' automated-networks/ansible-playbook/hosts)
-    ssh -i "$key_destino" ubuntu@"$ip"
-  esac
+    if [ "$topologia" == "3" ] || [ "$topologia" == "03" ]; then
+        generate_python_code "$topologia" "$num_switches_lvl1" "$num_switches_lvl2" "$num_hosts_per_switch_lvl2"
+    else
+        generate_python_code "$topologia" "$num_switches" "$num_hosts"
+    fi
 
-  case $iac in
-  2|02)
-    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl > /dev/null 2>&1
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - > /dev/null 2>&1
-    echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null 2>&1
-    sudo apt-get install -y terraform > /dev/null 2>&1
-    sed -i -e "4s|.*|  default = \"$aws_access_key\"|" -e "10s|.*|  default = \"$aws_secret_key\"|" -e "16s|.*|  default = \"$aws_session_token\"|" "$arquivo_destino_terraform"
-    terraform -chdir=automated-networks/terraform init
-    terraform -chdir=automated-networks/terraform apply -auto-approve
-  esac
+    case $iac in
+    1|01)
+        echo -e "\n\033[1;33m- [ Iniciando configurações da Infraestrutura. Aguarde! ] \033[0m\n"
+        sudo apt update -y > /dev/null 2>&1
+        sudo apt install git python3 python3-pip ansible -y > /dev/null 2>&1
+        pip install boto3 ansible-core==2.16.0 Jinja2==3.1.3 urllib3==1.26.5 > /dev/null 2>&1
+        ansible-galaxy collection install community.aws > /dev/null 2>&1
+        awk -v new_value_1="$aws_access_key" 'NR == 2 {print "aws_access_key: " new_value_1} NR != 2' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
+        awk -v new_value_2="$aws_secret_key" 'NR == 3 {print "aws_secret_key: " new_value_2} NR != 3' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
+        awk -v new_value_3="$aws_session_token" 'NR == 4 {print "aws_session_token: " new_value_3} NR != 4' "$arquivo_destino" > tmpfile && mv tmpfile "$arquivo_destino"
+        echo -e "\033[1;32m- [ Dependências instaladas com Sucesso! ] \033[0m\n"
+        ansible-playbook -i automated-networks/ansible-playbook/hosts automated-networks/ansible-playbook/playbook.yaml
+        #ip=$(awk '/ansible_host/ {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}' automated-networks/ansible-playbook/hosts)
+        #ssh -i "$key_destino" ubuntu@"$ip"
+    esac
 
+    case $iac in
+    2|02)
+        if [ ! -f "$key_destino" ]; then
+            touch "$key_destino"
+        fi
+        sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl > /dev/null 2>&1
+        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - > /dev/null 2>&1
+        echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null 2>&1
+        sudo apt-get install -y terraform > /dev/null 2>&1
+        sed -i -e "4s|.*|  default = \"$aws_access_key\"|" -e "10s|.*|  default = \"$aws_secret_key\"|" -e "16s|.*|  default = \"$aws_session_token\"|" "$arquivo_destino_terraform"
+        terraform -chdir=automated-networks/terraform init
+        terraform -chdir=automated-networks/terraform apply -auto-approve
+    esac
+
+elif [[ "$confirmacao" =~ ^(2|02)$ ]]; then
+    echo -e "\n\033[1;33m- [ Em desenvolvimento... ] \033[0m"
+else
+    echo -e "\n\033[1;33m- [ Desculpe, valor não encontrado, encerrando terminal... ] \033[0m"
 fi
