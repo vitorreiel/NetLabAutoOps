730915f9ea3215a7c52ad0500d5d44e4e712b666
vitorreiel <vitorreiel@alu.ufc.br>
2024-09-21 22:50:37 -0300

    feat: onos controller implementation

--- utils/network-components/start.sh
+++ utils/network-components/start.sh
@@ -3,6 +3,10 @@
 dest="/opt/network-components"
 docker build "${dest}/docker-images/alpine-image/" -t alpine-user
 docker build "${dest}/docker-images/containernet-image/" -t containernet
+docker build "${dest}/docker-images/onos-image/" -t onos-controller
 
-# [ Start ]
+# [ Start - ONOS - CONTROLLER ]
+docker run -d --name onos --restart always -p 8181:8181 -p 6633:6633 -p 6653:6653 onos-controller
+
+# [ Start - CONTAINERNET - TOPOLOGY ]
 docker run -d --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source="${dest}/topology",target=/netlabautoops containernet python3 /netlabautoops/topology.py
